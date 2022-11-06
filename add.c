2bd635fd754dce6bf525790f4ba1fe919b103808
aaryan1022 <104364223+aaryan1022@users.noreply.github.com>
2022-11-07 04:58:31 +0530

    simple add program

---
+++ add.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int a;
+    int b;
+    printf("enter number 1: ");
+    scanf("%d", &a);
+    printf("enter number 2: ");
+    scanf("%d", &b);
+    printf("Your sum is: %d", a+b);
+    return 0;
+}
